2e7f0e1047437b173124f16a9167dc2db51d62d6
vgerasimov <vgerasimov@quantumbrains.com>
2024-06-08 23:11:00 +0400

    [FastAPI] extractor selection with config added

--- src/dependencies/__init__.py
+++ src/dependencies/__init__.py
@@ -1,3 +1,7 @@
-from src.dependencies.extractors import get_pos_extractor
+from src.dependencies.extractors import (
+    get_bert_extractor,
+    get_llm_extractor,
+    get_pos_extractor,
+)
 
-__all__ = ["get_pos_extractor"]
+__all__ = ["get_pos_extractor", "get_llm_extractor", "get_bert_extractor"]
--- src/fastapi_app.py
+++ src/fastapi_app.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from src.dependencies.extractors import get_bert_extractor
 from src.endpoints.extract_objects import router
 
-app = FastAPI(title="model_explorer_api")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    get_bert_extractor()
+    yield
+
+
+app = FastAPI(title="model_explorer_api", lifespan=lifespan)
 
 
 @app.get("/health", tags=["health"])
