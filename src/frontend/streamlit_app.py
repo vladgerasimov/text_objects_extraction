520a1b1de21f2a4b257a877fb211199ca4f61a2e
Vlad <68383954+vladgerasimov@users.noreply.github.com>
2024-06-09 12:17:19 +0400

    [image generator] DALL-E added (#14)
    * [frontend, dall-e API] images generation added
    
    * [deploy] docker-compose updated
    
    ---------
    
    Co-authored-by: vgerasimov <vgerasimov@quantumbrains.com>

--- src/frontend/streamlit_app.py
+++ src/frontend/streamlit_app.py
@@ -2,11 +2,15 @@ import streamlit as st
 
 from src.core.settings import CONFIGS_DIR, ExtractorClientSettings
 from src.extractor.client import ExtractorClient
+from src.image_generator import DallEGenerator, DallESettings
 
 config = ExtractorClientSettings.from_yaml(
     config_path=CONFIGS_DIR / "extractor_client_settings.yaml", common_settings_path=CONFIGS_DIR / "app_settings.yaml"
 )
 client = ExtractorClient(config=config)
+
+config_generator = DallESettings.from_yaml(CONFIGS_DIR / "dall_e_settings.yaml")
+image_generator = DallEGenerator(config=config_generator)
 
 
 def extract_objects(text: str):
@@ -24,10 +28,20 @@ def display_extracted_objects():
         st.write("Extracted objects:", extracted_objects)  # pyright: ignore [reportUnknownMemberType]
 
 
+def display_generated_image():
+    if extracted_objects := st.session_state.get("extracted_objects"):
+        image_button = st.button("Generate image")
+        if image_button:
+            with st.spinner("Generating image..."):
+                generated_url = image_generator.get_generated_url(input_json=extracted_objects)
+            st.image(generated_url, caption="Generated image")
+
+
 def main():
     st.title("Object extraction")
     receive_user_input()
     display_extracted_objects()
+    display_generated_image()
 
 
 if __name__ == "__main__":
